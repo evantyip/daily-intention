62c500239c817fa8e7f2c2d9915ae9c9374d11ea
Evan Yip <etyip@ucsb.edu>
2021-12-12 21:48:08 -0800

    init auth setup

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  secret: process.env.SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+});
